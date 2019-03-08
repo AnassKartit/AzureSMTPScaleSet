049841b8ea13466ab33dc664a597b22bf2a52b28
Anass Kartit <anassk@gmail.com>
2019-03-08 16:34:33 +0100

    Update install_smtp.ps1

--- install_smtp.ps1
+++ install_smtp.ps1
@@ -1,2 +1,16 @@
 add-WindowsFeature "smtp-server"
+$disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
+
+$letters = 70..89 | ForEach-Object { [char]$_ }
+$count = 0
+$label = "datadisk"
+
+foreach ($disk in $disks) {
+    $driveLetter = $letters[$count].ToString()
+    $disk | 
+    Initialize-Disk -PartitionStyle MBR -PassThru |
+    New-Partition -UseMaximumSize -DriveLetter $driveLetter |
+    Format-Volume -FileSystem NTFS -NewFileSystemLabel "$label.$count" -Confirm:$false -Force
+$count++
+}
  
